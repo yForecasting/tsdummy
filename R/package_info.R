6c9851117c3399e83ff69861d17fabf1074458e6
Yves <yves.sagaert@gmail.com>
2025-01-15 22:19:05 +0100

    Update name of package

---
+++ R/package_info.R
@@ -0,0 +1,9 @@
+#' tsuntangle: Time Series Untangle Package
+#'
+#' This package creates clear formatting, extra features,
+#'  dummies for data with a date information or in time series format.
+#'
+#' @author  Yves R. Sagaert \email{yves.r.sagaert@gmail.com}
+#' @docType package
+#' @name tsdummy
+"_PACKAGE"
--- R/tsdummy_package.R
+++
@@ -1,9 +0,0 @@
-#' tsdummy: Time Series Dummy
-#'
-#' This package creates extra features for data with date information of in a
-#' time series format. Binary dummies or summarising feature engineering.
-#'
-#' @author  Yves R. Sagaert \email{yves.r.sagaert@gmail.com}
-#' @docType package
-#' @name tsdummy
-"_PACKAGE"
